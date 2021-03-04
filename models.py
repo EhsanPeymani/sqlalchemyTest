55739c317555f4929c84dbcba8da953ff5eae7dd
Ehsan Peymani <ehsan.peymani@nabors.com>
2021-03-05 00:36:30 +0100

    working versoin with backref

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, DateTime, Float
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -21,7 +21,39 @@ class Employee(Base):
     Phone = Column(String)
     Fax = Column(String)
     Email = Column(String)
-    Reporters = relationship("Employee", backref=backref("employees"))
+    Customers = relationship("Customer", backref=backref("employees"))
+    # Reporters = relationship("Employee", backref=backref("employees"))
+
+class Customer(Base):
+    __tablename__ = "customers"
+    CustomerId = Column(Integer, primary_key=True)
+    LastName = Column(String)
+    FirstName = Column(String)
+    Company = Column(String)
+    Address = Column(String)
+    City = Column(String)
+    State = Column(String)
+    Country = Column(String)
+    PostalCode = Column(String)
+    Phone = Column(String)
+    Fax = Column(String)
+    Email = Column(String)
+    SupportRepId = Column(Integer, ForeignKey("employees.EmployeeId"))
+    Invoices = relationship("Invoice", backref=backref("Customer"))
+
+class Invoice(Base):
+    __tablename__ = "invoices"
+    InvoiceId = Column(Integer, primary_key=True)
+    CustomerId = Column(Integer, ForeignKey("customers.CustomerId"))
+    InvoiceDate = Column(DateTime)
+    BillingAddress = Column(String)
+    BillingCity = Column(String)
+    BillingState = Column(String)
+    BillingCountry = Column(String)
+    BillingPostalCode = Column(String(10))
+    Total = Column(Float)
+
+
 
 class Genre(Base):
     __tablename__ = "genres"
--- server.py
+++ server.py
@@ -18,9 +18,18 @@ def main():
 
     print("Employee Reporters: ")
     for emp in data:
-        print(f"{emp.FirstName} {emp.LastName} has  reporters.")
+        print(f"{emp.FirstName} {emp.LastName} has {len(emp.Customers)} customers.")
     
+    print("------------------------------------")
+    data = session.query(Customer).all()
+    for cust in data:
+        p = [str(invoice.InvoiceId) for invoice in cust.Invoices]
+        print(f"Customer {cust.CustomerId}: Invoice Ids {p}")
 
+    print("------------------------------------")
+    invoice = session.query(Invoice).filter_by(InvoiceId=10).first()
+    print(f"Invoice {invoice.InvoiceId} relates to Customer {invoice.Customer.LastName}")     
 
 if __name__ == "__main__":
     main()
+
